9e0bf52b34d5382e4919aee4d53fa8bb3649dcc3
CoderPraveen93 <praveenku1885@gmail.com>
2026-03-19 23:25:10 +0530

    complete chapter 1

--- first_class.py
+++ first_class.py
@@ -1,1 +1,92 @@
-print("Hello World")
+
+#!operators
+
+#! 1. arithmetic operator
+
+
+# a=11
+# b=3
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b) #! power operator (a^b)
+
+#! 2. ralational/comparison operator
+# (==, !=, <=, >=, <, >)
+
+# a=1
+# b=2
+# print(a==b)#False
+
+
+#! 3. assignment operators
+#(+=, -=, *=, /=, %=, **=)
+# a = 10
+# a**=2
+# print(a)
+
+#! 4. logical operators
+#(not, and, or)
+
+# val1 = True
+# val2 = False
+
+# print("ans of operator : ", not(val1 and val2))
+# print("ans of operator :", val1 or val2)
+
+
+#! type conversion
+
+# a = 10  #int
+# b = 10.0 #float
+
+# print (a+b)
+
+#! type casting
+
+# a = "2"
+# b = 4
+
+# print(float(a) + b)
+
+#! take input from the user
+
+
+# value = int(input("enter value: "))
+
+# print(type(value), value)
+
+#! write a program, take 2 number from input and print their sum
+
+# num1 = int(input("enter num1:"))
+# num2 = float(input("enter num2:"))
+
+# print("the sum = ", num1+num2, "and type of the sum = ", type(num1+num2))
+
+
+#! write a program to print area of a square, take length and width from the user as input
+
+# side = int(input("enter length = "))
+
+
+# print("area of sqaure = ", side*side)
+
+#! take 2 value by input and print their avarage
+
+
+# valu1 = float(input("enter first value = "))
+# valu2 = float(input("enter second value = "))
+
+# print("the avarage = ", ((valu1+valu2)/2))
+
+
+#! take 2 number from input if first number >= second number then print True otherwise print False
+
+
+a = float(input("enter first numer:"))
+b = float(input("enter second numer:"))
+
+print(a >= b)
